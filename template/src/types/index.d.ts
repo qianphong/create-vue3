a498df5a50f64cfcdbb96d13030c54fb8d0fbdb2
qianphong <qianphong@163.com>
2021-11-30 10:26:18 +0800

    style: type

--- template/src/types/index.d.ts
+++ template/src/types/index.d.ts
@@ -1,9 +1,7 @@
 import type { App } from 'vue'
 import type { Router } from 'vue-router'
 
-interface Context {
+export type UserModule = (ctx: {
   app: App<Element>
   router: Router
-}
-
-export type UserModule = (ctx: Context) => void
+}) => void
